3cf871d236992d9a32cab7e0c8435c86b8d93a57
Vamsi Duvvuri <vamsi.duvvuri@gmail.com>
2017-11-28 20:49:24 -0500

    Fixed naked twins problem. Now have to revert to Udacity's code for search, only_choice and eliminate function to them re-implement the diagonal constraint correctly.

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+from time import gmtime, strftime
+
 assignments = []
 rows = 'ABCDEFGHI'
 cols = '123456789'
@@ -5,79 +7,50 @@ cols = '123456789'
 def cross(A, B):
 	return [s+t for s in A for t in B]
 
+naked_twin_test_problem = {'A1':'2345689', 'A2':'234568', 'A3':'7',
+'A4':'34568', 'A5':'1', 'A6':'23689', 'A7':'234568', 'A8':'345689',
+'A9':'2458', 'B1':'2345689', 'B2':'1234568', 'B3':'1234569', 'B4':'345678',
+'B5':'23456789', 'B6':'236789', 'B7':'2345678', 'B8':'3456789', 'B9':'124578',
+'C1':'2345689', 'C2':'1234568', 'C3':'1234569', 'C4':'345678', 'C5':'23456789',
+'C6':'236789', 'C7':'2345678', 'C8':'3456789', 'C9':'124578', 'D1':'36',
+'D2':'9', 'D3':'8', 'D4':'1', 'D5':'36', 'D6':'5', 'D7':'47', 'D8':'2',
+'D9':'47', 'E1':'567', 'E2':'567', 'E3':'56', 'E4':'2', 'E5':'678', 'E6':'4',
+'E7':'9', 'E8':'1', 'E9':'3', 'F1':'1', 'F2':'24', 'F3':'24', 'F4':'9',
+'F5':'37', 'F6':'37', 'F7':'58', 'F8':'58', 'F9':'6', 'G1':'23456789',
+'G2':'12345678', 'G3':'1234569', 'G4':'345678', 'G5':'23456789',
+'G6':'1236789', 'G7':'2345678', 'G8':'345678', 'G9':'24578', 'H1':'23456789',
+'H2':'12345678', 'H3':'1234569', 'H4':'345678', 'H5':'23456789',
+'H6':'1236789', 'H7':'2345678', 'H8':'345678','H9':'24578', 'I1':'2345678',
+'I2':'2345678', 'I3':'23456', 'I4':'345678', 'I5':'2345678', 'I6':'23678',
+'I7':'1', 'I8':'345678', 'I9':'9'}
+
+naked_twin_test_solution = {'A1':'2345689', 'A2':'234568', 'A3':'7',
+'A4':'34568', 'A5':'1', 'A6':'23689', 'A7':'234568', 'A8':'345689',
+'A9':'2458', 'B1':'2345689', 'B2':'1234568', 'B3':'1234569', 'B4':'345678',
+'B5':'23456789', 'B6':'236789', 'B7':'2345678', 'B8':'3456789', 'B9':'124578',
+'C1':'2345689', 'C2':'1234568', 'C3':'1234569', 'C4':'345678', 'C5':'23456789',
+'C6':'236789', 'C7':'2345678', 'C8':'3456789', 'C9':'124578', 'D1':'36',
+'D2':'9', 'D3':'8', 'D4':'1', 'D5':'6', 'D6':'5', 'D7':'47', 'D8':'2',
+'D9':'47', 'E1':'567', 'E2':'567', 'E3':'56', 'E4':'2', 'E5':'68', 'E6':'4',
+'E7':'9', 'E8':'1', 'E9':'3', 'F1':'1', 'F2':'24', 'F3':'24', 'F4':'9',
+'F5':'37', 'F6':'37', 'F7':'58', 'F8':'58', 'F9':'6', 'G1':'23456789',
+'G2':'12345678', 'G3':'1234569', 'G4':'345678', 'G5':'23456789',
+'G6':'1236789', 'G7':'2345678', 'G8':'345678', 'G9':'24578', 'H1':'23456789',
+'H2':'12345678', 'H3':'1234569', 'H4':'345678', 'H5':'23456789',
+'H6':'1236789', 'H7':'2345678', 'H8':'345678','H9':'24578', 'I1':'2345678',
+'I2':'2345678', 'I3':'23456', 'I4':'345678', 'I5':'2345678', 'I6':'23678',
+'I7':'1', 'I8':'345678', 'I9':'9'}
+
+
 boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-diagonal_units = [['A1','B2','C3','D4','E5','F6','G7','H8','I9'],['A9','B8','C7','D6','E5','F4','G3','H2','I1']]
+diagonal_units = [[r+c for r, c in zip(rows, cols)],[r + c for r, c in zip(rows, cols[::-1])]]
+#diagonal_units = [['A1','B2','C3','D4','E5','F6','G7','H8','I9'],['A9','B8','C7','D6','E5','F4','G3','H2','I1']]
 unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
-
-before_naked_twins_1 = {'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8',
-						'H5': '6', 'F9': '7', 'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8',
-						'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23', 'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5',
-						'G8': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'A4': '2357', 'A7': '27',
-						'A6': '257', 'C3': '8', 'C2': '237', 'C1': '23', 'E6': '579', 'C7': '9', 'C6': '6',
-						'C5': '37', 'C4': '4', 'I9': '9', 'D8': '8', 'I8': '7', 'E4': '6', 'D9': '6', 'H8': '2',
-						'F6': '125', 'A9': '8', 'G4': '9', 'A8': '6', 'E7': '345', 'E3': '379', 'F1': '6',
-						'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'E2': '37', 'F7': '35', 'F8': '9',
-						'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17', 'D3': '2379', 'B4': '27',
-						'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'D6': '279',
-						'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
-possible_solutions_1 = [
-	{'G7': '6', 'G6': '3', 'G5': '2', 'G4': '9', 'G3': '1', 'G2': '8', 'G1': '7', 'G9': '5', 'G8': '4', 'C9': '1',
-	 'C8': '5', 'C3': '8', 'C2': '237', 'C1': '23', 'C7': '9', 'C6': '6', 'C5': '37', 'A4': '2357', 'A9': '8',
-	 'A8': '6', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'F6': '125', 'F7': '35', 'F8': '9',
-	 'F9': '7', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'C4': '4',
-	 'B8': '3', 'B9': '4', 'I9': '9', 'I8': '7', 'I1': '23', 'I3': '23', 'I2': '6', 'I5': '5', 'I4': '8', 'I7': '1',
-	 'I6': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'E8': '1', 'A7': '27', 'A6': '257', 'E5': '347',
-	 'E4': '6', 'E7': '345', 'E6': '579', 'E1': '8', 'E3': '79', 'E2': '37', 'H8': '2', 'H9': '3', 'H2': '9',
-	 'H3': '5', 'H1': '4', 'H6': '17', 'H7': '8', 'H4': '17', 'H5': '6', 'D8': '8', 'D9': '6', 'D6': '279',
-	 'D7': '34', 'D4': '237', 'D5': '347', 'D2': '1', 'D3': '79', 'D1': '5'},
-	{'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8', 'H5': '6', 'F9': '7',
-	 'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8', 'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23',
-	 'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5', 'G8': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9',
-	 'A4': '2357', 'A7': '27', 'A6': '257', 'C3': '8', 'C2': '237', 'C1': '23', 'E6': '579', 'C7': '9', 'C6': '6',
-	 'C5': '37', 'C4': '4', 'I9': '9', 'D8': '8', 'I8': '7', 'E4': '6', 'D9': '6', 'H8': '2', 'F6': '125',
-	 'A9': '8', 'G4': '9', 'A8': '6', 'E7': '345', 'E3': '79', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235',
-	 'F5': '8', 'E2': '3', 'F7': '35', 'F8': '9', 'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17',
-	 'D3': '79', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6',
-	 'D6': '279', 'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
-	]
-
-before_naked_twins_2 = {'A1': '23', 'A2': '4', 'A3': '7', 'A4': '6', 'A5': '8', 'A6': '5', 'A7': '23', 'A8': '9',
-						'A9': '1', 'B1': '6', 'B2': '9', 'B3': '8', 'B4': '4', 'B5': '37', 'B6': '1', 'B7': '237',
-						'B8': '5', 'B9': '237', 'C1': '23', 'C2': '5', 'C3': '1', 'C4': '23', 'C5': '379',
-						'C6': '2379', 'C7': '8', 'C8': '6', 'C9': '4', 'D1': '8', 'D2': '17', 'D3': '9',
-						'D4': '1235', 'D5': '6', 'D6': '237', 'D7': '4', 'D8': '27', 'D9': '2357', 'E1': '5',
-						'E2': '6', 'E3': '2', 'E4': '8', 'E5': '347', 'E6': '347', 'E7': '37', 'E8': '1', 'E9': '9',
-						'F1': '4', 'F2': '17', 'F3': '3', 'F4': '125', 'F5': '579', 'F6': '279', 'F7': '6',
-						'F8': '8', 'F9': '257', 'G1': '1', 'G2': '8', 'G3': '6', 'G4': '35', 'G5': '345',
-						'G6': '34', 'G7': '9', 'G8': '27', 'G9': '27', 'H1': '7', 'H2': '2', 'H3': '4', 'H4': '9',
-						'H5': '1', 'H6': '8', 'H7': '5', 'H8': '3', 'H9': '6', 'I1': '9', 'I2': '3', 'I3': '5',
-						'I4': '7', 'I5': '2', 'I6': '6', 'I7': '1', 'I8': '4', 'I9': '8'}
-possible_solutions_2 = [
-	{'A1': '23', 'A2': '4', 'A3': '7', 'A4': '6', 'A5': '8', 'A6': '5', 'A7': '23', 'A8': '9', 'A9': '1', 'B1': '6',
-	 'B2': '9', 'B3': '8', 'B4': '4', 'B5': '37', 'B6': '1', 'B7': '237', 'B8': '5', 'B9': '237', 'C1': '23',
-	 'C2': '5', 'C3': '1', 'C4': '23', 'C5': '79', 'C6': '79', 'C7': '8', 'C8': '6', 'C9': '4', 'D1': '8',
-	 'D2': '17', 'D3': '9', 'D4': '1235', 'D5': '6', 'D6': '237', 'D7': '4', 'D8': '27', 'D9': '2357', 'E1': '5',
-	 'E2': '6', 'E3': '2', 'E4': '8', 'E5': '347', 'E6': '347', 'E7': '37', 'E8': '1', 'E9': '9', 'F1': '4',
-	 'F2': '17', 'F3': '3', 'F4': '125', 'F5': '579', 'F6': '279', 'F7': '6', 'F8': '8', 'F9': '257', 'G1': '1',
-	 'G2': '8', 'G3': '6', 'G4': '35', 'G5': '345', 'G6': '34', 'G7': '9', 'G8': '27', 'G9': '27', 'H1': '7',
-	 'H2': '2', 'H3': '4', 'H4': '9', 'H5': '1', 'H6': '8', 'H7': '5', 'H8': '3', 'H9': '6', 'I1': '9', 'I2': '3',
-	 'I3': '5', 'I4': '7', 'I5': '2', 'I6': '6', 'I7': '1', 'I8': '4', 'I9': '8'},
-	{'A1': '23', 'A2': '4', 'A3': '7', 'A4': '6', 'A5': '8', 'A6': '5', 'A7': '23', 'A8': '9', 'A9': '1', 'B1': '6',
-	 'B2': '9', 'B3': '8', 'B4': '4', 'B5': '3', 'B6': '1', 'B7': '237', 'B8': '5', 'B9': '237', 'C1': '23',
-	 'C2': '5', 'C3': '1', 'C4': '23', 'C5': '79', 'C6': '79', 'C7': '8', 'C8': '6', 'C9': '4', 'D1': '8',
-	 'D2': '17', 'D3': '9', 'D4': '1235', 'D5': '6', 'D6': '237', 'D7': '4', 'D8': '27', 'D9': '2357', 'E1': '5',
-	 'E2': '6', 'E3': '2', 'E4': '8', 'E5': '347', 'E6': '347', 'E7': '37', 'E8': '1', 'E9': '9', 'F1': '4',
-	 'F2': '17', 'F3': '3', 'F4': '125', 'F5': '579', 'F6': '279', 'F7': '6', 'F8': '8', 'F9': '257', 'G1': '1',
-	 'G2': '8', 'G3': '6', 'G4': '35', 'G5': '345', 'G6': '34', 'G7': '9', 'G8': '27', 'G9': '27', 'H1': '7',
-	 'H2': '2', 'H3': '4', 'H4': '9', 'H5': '1', 'H6': '8', 'H7': '5', 'H8': '3', 'H9': '6', 'I1': '9', 'I2': '3',
-	 'I3': '5', 'I4': '7', 'I5': '2', 'I6': '6', 'I7': '1', 'I8': '4', 'I9': '8'}
-]
-
 
 def assign_value(values, box, value):
 	"""
@@ -152,6 +125,7 @@ def eliminate(values):
 	"""
 	#print("In eliminate")
 	row=['A','B','C','D','E','F','G','H','I']
+
 	# for each box in the dictionary
 	for box, value in values.items():
 		# if the box is solved
@@ -178,6 +152,82 @@ def eliminate(values):
 					new_peer_value = old_peer_value.replace(value,"")
 					assign_value(values, peer, new_peer_value)
 	return values
+
+def naked_twins(values):
+	"""Eliminate values using the naked twins strategy.
+	Args:
+		values(dict): a dictionary of the form {'box_name': '123456789', ...}
+	Returns:
+		the values dictionary with the naked twins eliminated from peers.
+	"""
+	row = ['A','B','C','D','E','F','G','H','I']
+	traversed = []
+	for box, value in values.items():
+		# if the box is not solved and value is of length two [because naked twins requires two boxes to have values of length 2]
+		if len(value) == 2:
+			# find box's row and column unit index
+			box_row_idx = rows.index(box[:1])
+			box_column_idx = int(box[1:]) - 1
+
+			# find box's square unit index
+			for idx, unit in enumerate(square_units):
+				if box in unit:
+					box_square_idx = idx
+					break
+
+			# create list of peers using only the unit index for the box
+			row_peers = row_units[box_row_idx]
+			column_peers = column_units[box_column_idx]
+			square_peers = square_units[box_square_idx]
+
+			diagonal_peers = []
+			if box in diagonal_units[0]:
+				diagonal_peers = diagonal_units[0]
+			elif box in diagonal_units[1]:
+				diagonal_peers = diagonal_units[1]
+
+			all_peers = column_peers + row_peers + square_peers + diagonal_peers
+
+			# for box's peers
+			for peer in all_peers:
+				if box != peer and values[box] == values[peer] and [box, peer] not in traversed:
+					# don't do redundant traversing
+					traversed.append([box, peer])
+					traversed.append([peer, box])
+					#print("\nfound naked twin", box, peer, values[box])
+
+					# find all new peers only in the section relevant for the twins
+					new_all_peers = []
+					if box in column_peers and peer in column_peers:
+						new_all_peers = new_all_peers + column_peers
+
+					if box in row_peers and peer in row_peers:
+						new_all_peers = new_all_peers + row_peers
+
+					if box in square_peers and peer in square_peers:
+						new_all_peers = new_all_peers + square_peers
+
+					if box in diagonal_peers and peer in diagonal_peers:
+						new_all_peers = new_all_peers + diagonal_peers
+
+					for new_peer in new_all_peers:
+						if new_peer == box or new_peer == peer:
+							#print("\t", box, peer, new_peer, "box or original peer")
+							continue
+						elif len(values[new_peer]) < len(values[box]):
+							#print("\t", new_peer, values[new_peer]," value smaller")
+							continue
+						else:
+							#print("\t", new_peer, values[new_peer])
+							for s in values[box]:
+								if s in values[new_peer]:
+									#print("\treplacing:", new_peer, values[new_peer], values[new_peer].replace(s,""))
+									assign_value(values, new_peer, values[new_peer].replace(s,""))
+								#else:
+									#print("\t", new_peer, "[",values[new_peer],"]does not contain", s)
+
+	return values
+	# Eliminate the naked twins as possibilities for their peers
 
 def only_choice(values):
 	"""
@@ -225,11 +275,9 @@ def reduce_puzzle(values):
 		# Check how many boxes have a determined value
 		solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
 
-		# eliminate value
-		eliminate(values)
-
-		# prune using only choice
-		only_choice(values)
+		values = eliminate(values) # eliminate value
+		values = naked_twins(values) # implement naked twins
+		values = only_choice(values) # prune using only choice
 
 		# Check how many boxes have a determined value, to compare
 		solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
@@ -266,64 +314,7 @@ def search(values):
 		attempt = search(new_sudoku)
 		if attempt:
 			return attempt
-
-def naked_twins(values):
-	"""Eliminate values using the naked twins strategy.
-	Args:
-		values(dict): a dictionary of the form {'box_name': '123456789', ...}
-	Returns:
-		the values dictionary with the naked twins eliminated from peers.
-	"""
-	row = ['A','B','C','D','E','F','G','H','I']
-	#print("\nBefore:")
-	#display(values)
-	for box, value in values.items():
-		# if the box is solved
-		if len(value) > 1:
-			# find box's row and column unit index
-			box_row_idx = rows.index(box[:1])
-			box_column_idx = int(box[1:]) - 1
-
-			# find box's square unit index
-			for idx, unit in enumerate(square_units):
-				if box in unit:
-					box_square_idx = idx
-					break
-			# create list of peers using only the unit index for the box
-			#peers = list(set().union(row_units[box_row_idx], column_units[box_column_idx], square_units[box_square_idx]))
-			row_peers = row_units[box_row_idx]
-			column_peers = column_units[box_column_idx]
-			square_peers = square_units[box_square_idx]
-			all_peers = [column_peers, row_peers, square_peers]
-
-			# for box's peers
-			for set_of_peers in all_peers:
-				for peer in set_of_peers:
-					#if its value is equal to box's and it isn't the box itself
-					if box != peer and values[box] == values[peer]:
-						# found naked twin
-						# print("Found naked twin", box, peer)
-						for new_peer in set_of_peers:
-							if len(values[new_peer]) <= len(values[box]):
-								#print("length of new row peer value is not LTEQ box value")
-								continue
-							elif new_peer == box:
-								#print("same as naked twins")
-								continue
-							else:
-								#print("\nbox:",box, values[box], "row_peer:", row_peer, values[row_peer], "new_row_peer:", new_row_peer, values[new_row_peer])
-								count_in_peer = 0
-								for s in values[box]:
-									if s in values[new_peer]:
-#										count_in_peer += 1
-#								if count_in_peer == len(values[box]):
-#									for s in values[box]:
-										assign_value(values, new_peer, values[new_peer].replace(s,""))
-								#print("\tnew peer value", values[new_row_peer])
-	#print("\nAfter:")
-	#display(values)
 	return values
-	# Eliminate the naked twins as possibilities for their peers
 
 def solve(grid):
 	"""
@@ -334,32 +325,33 @@ def solve(grid):
 	Returns:
 		The dictionary representation of the final sudoku grid. False if no solution exists.
 	"""
-	#print("In solve")
+	values = search(grid_values(grid))
 
 	"""
-	values = grid_values(grid)
-	values = search(values)
-	return values
-
-	"""
-
 	print("\n\nOriginal Problem")
-	display(before_naked_twins_2)
-	print("\n\ncalling naked twins")
-	values = naked_twins(before_naked_twins_2)
+	display(naked_twin_test_problem)
+	values = naked_twins(naked_twin_test_problem)
 	print("\n\nPriting my solution")
 	display(values)
-	#print("\n\n")
-	print("\n\nPrinting correct solution #1")
-	display(possible_solutions_2[0])
-	print("\n\nPrinting correct solution #2")
-	display(possible_solutions_2[1])
-	print("\n\nDone printing")
+	print("\n\nPriting naked_twin_test_solution")
+	display(naked_twin_test_solution)
+	print("\n\nPriting my solution again\n")
+	"""
+
 	return values
 
 if __name__ == '__main__':
+	print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+
+	# original problem
 	#diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+
+	# grid to solve for diagnoal problem
 	diag_sudoku_grid = '9.1....8.8.5.7..4.2.4....6...7......5..............83.3..6......9................'
+
+	# grid recived from error when submitting using 'udacity submit'
+	#diag_sudoku_grid = '..7.1.......................981.5.2....2.49131..9....6........................1.9'
+
 	display(solve(diag_sudoku_grid))
 	try:
 		from visualize import visualize_assignments
